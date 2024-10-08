7ef7ba3fc4eaf9895b72142b577fd11e3eea2be0
jkerick87 <jkerick@gmail.com>
2024-10-08 11:36:34 -0300

    Primeira versão do projeto
    Projeto Banco Senac

---
+++ projeto/operacoes.py
@@ -0,0 +1,91 @@
+import json
+import menu
+
+    
+def carregar_contas():
+    try:
+        with open('contas.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError: 
+        return {}
+    except json.JSONDecodeError: 
+        return {}
+    
+def salvar_arquivo(contas):
+    with open('contas.txt', 'w') as file:
+        json.dump(contas, file)
+
+def criar_conta(contas):
+    nome = input("Digite o nome do titular: ")
+    numero_conta = input("Digite o número da conta: ")
+    saldo = float(input("Digite o saldo inicial: "))
+
+    contas[numero_conta] = {
+        "nome": nome,
+        "saldo": saldo
+    }
+
+    salvar_arquivo(contas)
+    print("Conta criada com sucesso!")
+
+def consultar_conta(contas):
+    numero_conta = input("Digite o número da conta para consulta: ")
+    if numero_conta in contas:
+        conta = contas[numero_conta]
+        print(f"Nome: {conta['nome'].upper()}, Saldo: {conta['saldo']:.2f}")
+    else:
+        print("Conta não encontrada.")
+
+def depositar_dinheiro(contas):
+    conta_alvo = input("Digite a conta: ")
+    
+    if conta_alvo in contas :
+        novo_valor = float(input("Digite o valor do deposito:"))
+        if novo_valor > 0:
+            contas[conta_alvo]['saldo'] += novo_valor
+            print(f"Depósito de {novo_valor} realizado com sucesso")
+            salvar_arquivo(contas)
+        else:
+            print("Valor inválido. O depósito deve ser maior que zero.")
+    else:
+        print("Conta não encontrada.")
+        
+
+def sacar_dinheiro(contas):
+   conta_alvo = input("Digite a conta: ")
+   if conta_alvo in contas :
+        novo_valor = float(input("Digite o valor do saque:"))
+        if contas[conta_alvo]['saldo'] > novo_valor:
+            contas[conta_alvo]['saldo'] -= novo_valor
+            print(f"Saque de {novo_valor} realizado com sucesso")
+            salvar_arquivo(contas)
+        else:
+            print("Saldo Insuficiente!!!")
+   else:
+    print("Conta não encontrada.")
+       
+def main():
+    contas = carregar_contas()
+
+    while True:
+        menu.menu()
+        opcao = input("Escolha uma opção: ")
+        
+        if opcao == '1':
+            criar_conta(contas)
+
+        elif opcao == '2':
+            consultar_conta(contas)
+              
+        elif opcao == '3':
+            depositar_dinheiro(contas)
+        
+        elif opcao == '4':
+            sacar_dinheiro(contas) 
+            
+        elif opcao == '5':
+            print("Saindo...")
+            break
+        
+        else:
+            print("Opção inválida, tente novamente.")
